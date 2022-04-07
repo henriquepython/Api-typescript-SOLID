806b729f57c3e74d901e2ac3fb58704304c1cca4
Henriquepython <joaohbp59@gmail.com>
2022-04-07 14:19:35 -0300

    update

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,10 @@
-import { request, response, Router } from "express";
+import { Request, Response, Router } from "express";
 import { createUserController } from "./useCases/createUser";
 
 const router = Router();
 
-router.post('/users', (request, response) => {
+router.post('/users', (request: Request, response: Response) => {
     return createUserController.handle(request, response);
 });
 
-export {router}
+export {    router  }
